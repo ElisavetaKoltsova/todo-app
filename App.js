8173c529b800d31975034f3692b2d76b66a3d0d1
lisakolts <crokodile.liza@gmail.com>
2024-09-24 17:38:40 +0300

    Delete to do element

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { FlatList, StyleSheet, View, Text } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 import Header from './components/Header';
 import List from './components/List';
 import { useState } from 'react';
@@ -7,29 +7,39 @@ export default function App() {
   const [listOfToDo, setListOfToDo] = useState([
     {
       text: 'Bye ice cream',
-      id: 1
+      id: 0
     },
     {
       text: 'Do homework',
-      id: 2
+      id: 1
     },
     {
       text: 'Cook diner',
-      id: 3
+      id: 2
     },
     {
       text: 'Meet with friends',
-      id: 4
+      id: 3
     },
   ]);
+
+  handlePressDeleteButton = (listElement) => {
+    const copyListOfToDo = [...listOfToDo];
+    delete copyListOfToDo[listElement.id];
+    setListOfToDo(copyListOfToDo);
+  };
 
   return (
     <View style={styles.main}>
       <Header />
-      <View>
+      <View style={styles.flatList}>
         <FlatList
           data={listOfToDo}
-          renderItem={({item}) => (<List listElement={item} />)}
+          renderItem={({item}) => (
+            <List
+              listElement={item}
+              onPress={handlePressDeleteButton}/>
+          )}
         />
       </View>
     </View>
@@ -39,5 +49,8 @@ export default function App() {
 const styles = StyleSheet.create({
   main: {
     width: '100%'
+  },
+  flatList: {
+    justifyContent: 'center'
   }
 });
--- components/List.js
+++ components/List.js
@@ -1,13 +1,45 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 
-export default function List({listElement}) {
-  return (
-    <View style={styles.container}>
-      <Text>{listElement.text}</Text>
-    </View>
-  );
+export default function List({listElement, onPress}) {
+  if (listElement) {
+    return (
+      <View style={styles.main}>
+        {console.log(listElement)}
+        <Text style={styles.text}>{listElement.text}</Text>
+        <TouchableOpacity onPress={() => onPress(listElement)}>
+          <Text style={styles.button}>X</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+  return '';
 }
 
 const styles = StyleSheet.create({
-  
+  main: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    color: 'black',
+    borderWidth: 2,
+    borderBlockColor: 'black',
+    borderRadius: 25,
+    width: '80%',
+    justifyContent: 'space-between',
+    marginLeft: '10%',
+    marginTop: 10
+  },
+  text: {
+    textAlign: 'justify',
+    fontSize: 15,
+    marginLeft: 15
+  },
+  button: {
+    backgroundColor: 'black',
+    color: 'white',
+    borderRadius: 25,
+    marginRight: 15,
+    width: 20,
+    textAlign: 'center'
+  }
 });
